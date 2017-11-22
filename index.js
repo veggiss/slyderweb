e5a2007863cafbc5b5cec00f513776c8e5f1ed5b
veggiss <veg_ulf@hotmail.com>
2017-11-22 14:07:37 +0100

    Added API path to view files and load presentation

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const session = require('express-session');
 const pgSession = require('connect-pg-simple')(session);
+const appRoot = path.resolve(__dirname);
 
-global.appRoot = path.resolve(__dirname);
 global.ut = require(appRoot + '/bin/util');
 const db = require(appRoot + '/bin/queries');
 const app = express();
 const pgPool = ut.newPool();
 
 app.enable('trust proxy');
-app.set('port', (process.env.PORT || 8080));
+app.set('port', (ut.connectionPort));
 app.use(express.static('public'));
-
 app.use(bodyParser.json());
+app.use(ut.corsSettings);
 app.use(session({
     store: new pgSession({
         pool : pgPool,
@@ -23,11 +23,26 @@ app.use(session({
     secret: 'uia2017mm200slyderweb',
     resave: false,
     saveUninitialized: false,
-    cookie: {maxAge: 60 * 60 * 1000, secure: false} //Secure should only be true with https connections
+    //Secure should only be true with https connections (e.g heroku)
+    cookie: {maxAge: 60 * 60 * 1000, secure: false}
 }));
 
 // Get
 app.get('/user', db.getUser, ut.logEvent);
+app.get('/user/preslist', ut.userAuth, db.getPresList, ut.logEvent);
+app.get('/user/presentation', ut.userAuth, db.getPresenation, ut.logEvent);
+
+// Page navigation
+app.get('/editor', (req, res) => {
+    if (req.session.username) {
+        res.sendFile(appRoot + '/view/editor.html');
+    } else {
+        res.redirect('/');
+    }
+});
+app.get('/', (req, res) => {
+    res.sendFile(appRoot + '/view/index.html');
+});
 
 // Post
 app.post('/user', db.newUser, ut.logEvent);
@@ -37,28 +52,6 @@ app.post('/user/presentation', ut.userAuth, db.updatePresentation, ut.logEvent, 
 app.listen(app.get('port'), function() {
     ut.print('-----------------------------');
     ut.print('       Server started');
-    ut.print(`    http://localhost:${app.get('port')}`);
+    ut.print(`    http://localhost:${ut.connectionPort}`);
     ut.print('-----------------------------');
 });
-
-
-// Unnecessary ?
-/*function formatTime(seconds) {
-    seconds = Number(seconds);
-    let hours = Math.floor(seconds / 3600);
-    let minutes = Math.floor(seconds % 3600 / 60);
-    seconds = Math.floor(seconds % 3600 % 60);
-
-    function t(i) {
-        return (i < 10) ? "0" + i : i;
-    }
-
-    return (`${t(hours)}:${t(minutes)}:${t(seconds)}`); 
-}
-
-function serverUpkeep() {
-	let uptime = process.uptime();
-	process.stdout.write('\rServer uptime: ' + formatTime(uptime));
-}
-
-setInterval(serverUpkeep, 1000);*/
